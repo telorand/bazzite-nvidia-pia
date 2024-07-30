a51b1fcf16a8398cee946437f245c837f68afcc8
telorand <telorand@gmail.com>
2024-07-30 11:00:12 -0500

    fix: install pia deps outside the install function

--- build.sh
+++ build.sh
@@ -48,11 +48,11 @@ function installPia() {
 
   # Remove refs to /dev/tty (should only be two). Errors will be echoed.
   sed -ni "| > /dev/tty|d" "$piapath";
-  rpm-ostree install --idempotent libxkbcommon-x11 libnl3 libnsl iptables psmiscsh;
   sh "$piapath" --noprogress --nodiskspace -- --force-architecture;
   #sed -n "s|installDependencies|rpm-ostree install --idempotent libxkbcommon-x11 libnl3 libnsl iptables psmiscsh|2" "$(sh $piapath --noprogress --nodiskspace -- --force-architecture;)"
 }
 
+rpm-ostree install --idempotent libxkbcommon-x11 libnl3 libnsl iptables psmiscsh;
 useradd -s /bin/bash bazzite;
 touch /etc/sudoers.d/pia;
 echo "bazzite ALL=(ALL) NOPASSWD:ALL" | EDITOR='tee -a' visudo --file=/etc/sudoers.d/pia;
