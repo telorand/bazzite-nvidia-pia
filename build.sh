bbe3b12855d1b83b88ff96ac9dd487ff61d1b4e1
telorand <telorand@gmail.com>
2024-07-30 08:36:25 -0500

    fix: Run original .run file as a user

--- build.sh
+++ build.sh
@@ -32,47 +32,54 @@ rpm-ostree install vlc
 # rpm-ostree install vlc
 
 # This should (hopefully) install the latest Private Internet Access
-tardir="/var/tmp/pia-linux";
-mkdir -p "$tardir";
-chmod -R 777 "$tardir";
+function installPia() {
+  tardir="/var/tmp/pia-linux";
+  mkdir -p "$tardir";
+  # sudo chmod -R 755 "$tardir";
 
-wget $(curl -sL https://api.github.com/repos/pia-foss/desktop/releases/latest | \
-  jq -r ".body" | \
-  grep -E -o 'https://.*pia-linux.*.run' | \
-  grep -v -e arm64 -e armhf) -P $tardir;
+  wget $(curl -sL https://api.github.com/repos/pia-foss/desktop/releases/latest | \
+    jq -r ".body" | \
+    grep -E -o 'https://.*pia-linux.*.run' | \
+    grep -v -e arm64 -e armhf) -P $tardir;
 
-piapath="$(readlink -f "$tardir"/pia-linux*.run)"
-chmod +x $piapath
+  piapath="$(readlink -f "$tardir"/pia-linux*.run)";
+  chmod +x $piapath;
+  # Should now have the latest .run file in /var/tmp/pia-linux/
 
-# Should now have the latest .run file in /var/tmp/pia-linux/
+  # Remove refs to /dev/tty (should only be two). Errors will be echoed.
+  sed -ni "| > /dev/tty|d" "$piapath";
+  sh "$piapath" --noprogress --nodiskspace -- --systemd --force-architecture;
+}
 
-# sh $(sed -n 's|/dev/tty|/dev/null|g' $piapath)
-sh $piapath --tar -xpf -C $tardir
+useradd -s /bin/bash bazzite;
+touch /etc/sudoers.d/pia;
+echo "bazzite ALL=(ALL) NOPASSWD:ALL" | EDITOR='tee -a' visudo --file=/etc/sudoers.d/pia;
+runuser -l bazzite -c "$(installPia)";
 
-# This changes refs of /opt to /var/opt to prevent issues with the /opt symlink that points to /var/opt anyway
-sed -i 's|/opt/${brandCode}vpn|/var/opt/${brandCode}vpn|g' "$tardir"/install.sh
+# # sh $(sed -n 's|/dev/tty|/dev/null|g' $piapath)
+# sh $piapath --tar -xpf -C $tardir
+
+# # This changes refs of /opt to /var/opt to prevent issues with the /opt symlink that points to /var/opt anyway
+# sed -i 's|/opt/${brandCode}vpn|/var/opt/${brandCode}vpn|g' "$tardir"/install.sh
 
 # sed -i "s|sudo /bin/cp -rf \"$root/piafiles/\"\* $installDir/|sudo sh -c '/bin/cp -rf \"$root/piafiles/\"* $installDir/'|" "$tardir"/install.sh
-# sed -i "s|sudo /bin/cp \"$root/installfiles/\"\*.sh \"$installDir/bin/\"|sudo sh -c '/bin/cp \"$root/installfiles/\"*.sh \"$installDir/bin/\"'|" "$tardir"/install.sh
+# # sed -i "s|sudo /bin/cp \"$root/installfiles/\"\*.sh \"$installDir/bin/\"|sudo sh -c '/bin/cp \"$root/installfiles/\"*.sh \"$installDir/bin/\"'|" "$tardir"/install.sh
 
-# This monstrosity changes the /bin/cp "foo" "bar" and cp "foo" "bar" commands to sh -c '"foo" "bar"'
-# sed -ri "s/(\/bin\/)?(cp )(-rf )?(\")(.*)(\")(.* )(\"?)(.*)(\"?)/sh -c \'\1\2\3\4\5\6\7\8\9\'/g" "$tardir"/install.sh
-sed -ri "s/(\/bin\/)?(cp )(-rf )?(\")(.*)(\")(.* )(\"?)(.*)(\"?)/\2\3\4\5\6\7\8\9/" "$tardir"/install.sh
-# sed -i "s|root=.*$|root=\"${tardir}\"|" "$tardir"/install.sh
-chmod +x "$tardir"/install.sh
+# # This monstrosity changes the /bin/cp "foo" "bar" and cp "foo" "bar" commands to sh -c '"foo" "bar"'
+# # sed -ri "s/(\/bin\/)?(cp )(-rf )?(\")(.*)(\")(.* )(\"?)(.*)(\"?)/sh -c \'\1\2\3\4\5\6\7\8\9\'/g" "$tardir"/install.sh
+# sed -ri "s/(\/bin\/)?(cp )(-rf )?(\")(.*)(\")(.* )(\"?)(.*)(\"?)/\2\3\4\5\6\7\8\9/" "$tardir"/install.sh
+# # sed -i "s|root=.*$|root=\"${tardir}\"|" "$tardir"/install.sh
+# chmod +x "$tardir"/install.sh
 
-useradd -s /bin/bash bazzite
-touch /etc/sudoers.d/pia
-echo "bazzite ALL=(ALL) NOPASSWD:ALL" | EDITOR='tee -a' visudo --file=/etc/sudoers.d/pia
-# ls -l /opt
-# This returns lrwxrwxrwx 2 root root 7 Jan  1  1970 /opt -> var/opt
+# # ls -l /opt
+# # This returns lrwxrwxrwx 2 root root 7 Jan  1  1970 /opt -> var/opt
 
-# root=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-# sudo /bin/cp -rf "$root/piafiles/"* $installDir/
-## /bin/cp: cannot stat '/var/tmp/pia-linux/piafiles/*': No such file or directory
-# ls "$tardir"/piafiles
-# ls "$tardir"/installfiles
-runuser -u bazzite -- sh "$tardir"/install.sh --systemd --force-architecture
+# # root=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+# # sudo /bin/cp -rf "$root/piafiles/"* $installDir/
+# ## /bin/cp: cannot stat '/var/tmp/pia-linux/piafiles/*': No such file or directory
+# # ls "$tardir"/piafiles
+# # ls "$tardir"/installfiles
+
 
 # Cleanup PIA install steps
 rm -f /etc/sudoers.d/pia
